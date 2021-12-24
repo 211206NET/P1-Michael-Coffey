9206f29eaf14effa1a7273edd57c6e7bdfc61353
Michael Coffey <michaelcoffey1999@gmail.com>
2021-12-23 20:42:45 -0600

    BL for manager

---
+++ StoreBL/MBL.cs
@@ -0,0 +1,25 @@
+namespace BL;
+
+public class MBL{
+    private FMRepo _fdl;
+
+    public MBL(){
+        _fdl = new FMRepo();
+    }
+
+    public List<Storefront> GetAllStorefronts(){
+        return _fdl.GetAllStorefronts();
+    }
+
+    public List<Customers> GetAllCustomers(){
+        return _fdl.GetAllCustomers();
+    }
+
+    public void AddStorefront(Storefront storefrontToAdd){
+        _fdl.AddStorefront(storefrontToAdd);
+    }
+
+    public void ReplenishStock(int nInd, int nInv, int nAmo){
+        _fdl.ReplenishStock(nInd, nInv, nAmo);
+    }
+}
