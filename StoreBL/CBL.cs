3c26641b2c3a6076a493e7dfc00295bcdae54045
Michael Coffey <michaelcoffey1999@gmail.com>
2021-12-26 14:21:33 -0600

    Beginnings for Customer functions

---
+++ StoreBL/CBL.cs
@@ -0,0 +1,3 @@
+namespace BL;
+
+public class CBL {}
--- StoreUI/CustomerMenu.cs
+++ StoreUI/CustomerMenu.cs
@@ -3,5 +3,8 @@ namespace UI;
 public class CustomerMenu{
     public CustomerMenu(){}
 
-    public void Start(){}
+    public void Start(){
+        bool exit = false;
+        while(!exit){}
+    }
 }
