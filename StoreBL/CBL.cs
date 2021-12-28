f4210d3a934c21bac52fdc2bdf0a2f5af12bdfc7
Michael Coffey <michaelcoffey1999@gmail.com>
2021-12-27 18:11:21 -0600

    More Customer changes

--- StoreBL/CBL.cs
+++ StoreBL/CBL.cs
@@ -1,3 +1,17 @@
 namespace BL;
 
-public class CBL {}
+public class CBL {
+    private FCRepo _cdl;
+
+    public CBL(){
+        _cdl = new FCRepo();
+    }
+
+    public void GetOrderHistory(){
+        _cdl.GetOrderHistory();
+    }
+
+    public void PlaceAnOrder(Product newPro){
+        _cdl.PlaceAnOrder();
+    }
+}
--- StoreDL/FCRepo.cs
+++ StoreDL/FCRepo.cs
@@ -4,8 +4,6 @@ public class FCRepo : ICRepo {
     private string filePath = "../StoreDL/Customers.json";
     private string filePath2 = "../StoreDL/Storefronts.json";
 
-    public FCRepo(){}
-
     public void GetOrderHistory(){}
 
     public void PlaceAnOrder(Product newP){}
