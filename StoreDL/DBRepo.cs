4f5b3f5a7670db57a26d72fff67fcbb8bd3b90ff
Michael Coffey <michaelcoffey1999@gmail.com>
2022-01-06 14:25:59 -0600

    fixed the add functions

--- StoreDL/DBRepo.cs
+++ StoreDL/DBRepo.cs
@@ -110,7 +110,7 @@ public class DBRepo : IMRepo{
     public void AddCustomer(string _username, string _email, string _password, int _corderhistoryid){
         using(SqlConnection connection = new SqlConnection(_connectionString)){
             connection.Open();
-            string cmdSql = "INSERT INTO Customer (UserName, Email, Password, COrderHistoryID) VALUES (@usna, @emai, @ paswor, @coh)";
+            string cmdSql = "INSERT INTO Customer (UserName, Email, Password, COrderHistoryID) VALUES (@usna, @emai, @paswor, @coh)";
             using(SqlCommand cmd = new SqlCommand(cmdSql, connection)){
                 SqlParameter param = new SqlParameter("@usna", _username);
                 cmd.Parameters.Add(param);
