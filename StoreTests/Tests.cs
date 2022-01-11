3e13fc2f18d2139a7492fec27e4ef71569f50738
Michael Coffey <michaelcoffey1999@gmail.com>
2022-01-11 10:55:59 -0600

    new exceptions

--- StoreTests/Tests.cs
+++ StoreTests/Tests.cs
@@ -6,5 +6,34 @@ using CustomExceptions;
 using System.Collections.Generic;
 
 public class ModelTests{
-    
+    [Fact]
+    public void CustomerShouldCreate(){
+        Customer testCustomer = new Customer();
+        Assert.NotNull(testCustomer);
+    }
+    [Fact]
+    public void InventoryShouldCreate(){
+        Inventory testInvnetory = new Inventory();
+        Assert.NotNull(testInvnetory);
+    }
+    [Fact]
+    public void LineItemShouldCreate(){
+        LineItem testLineItem = new LineItem();
+        Assert.NotNull(testLineItem);
+    }
+    [Fact]
+    public void OrderShouldCreate(){
+        Order testOrder = new Order();
+        Assert.NotNull(testOrder);
+    }
+    [Fact]
+    public void ProductShouldCreate(){
+        Product testProduct = new Product();
+        Assert.NotNull(testProduct);
+    }
+    [Fact]
+    public void StorefrontShouldCreate(){
+        Storefront testStorefront = new Storefront();
+        Assert.NotNull(testStorefront);
+    }
 }
