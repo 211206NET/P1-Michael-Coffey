c7e6f40ddee8087cd1b53dcc1091f127caf4a427
Michael Coffey <michaelcoffey1999@gmail.com>
2022-01-11 22:53:54 -0600

    getting inventory

--- StoreUI/ManagerMenu.cs
+++ StoreUI/ManagerMenu.cs
@@ -116,9 +116,9 @@ public class ManagerMenu : IMenu{
                     Console.WriteLine("Enter the film's title");
                     string nTitle = Console.ReadLine();
                     Console.WriteLine("Enter the film's director");
-                    int nDirector = Int32.Parse(Console.ReadLine());
+                    int nDirector = Console.ReadLine();
                     Console.WriteLine("Enter a film's rating");
-                    int nRating = Int32.Parse(Console.ReadLine());
+                    int nRating = Console.ReadLine();
                     Console.WriteLine("Enter a film's release year");
                     int nYear = Int32.Parse(Console.ReadLine());
                     Console.WriteLine("Enter the price");
