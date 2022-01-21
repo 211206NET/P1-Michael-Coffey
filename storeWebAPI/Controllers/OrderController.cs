bf45911402550f246cb798d0567cd07dbccfa29d
Michael Coffey <michaelcoffey1999@gmail.com>
2022-01-21 15:53:11 -0600

    minor changes

--- storeWebAPI/Controllers/OrderController.cs
+++ storeWebAPI/Controllers/OrderController.cs
@@ -51,7 +51,7 @@ namespace storeWebAPI.Controllers
 
         // PUT api/<ValuesController>/5
         [HttpPut("{id}")]
-        public void PutIntoOrder(int id, [FromBody] string value)
+        public void PutIntoOrder(int id, [FromBody] Order nOrder)
         {
         }
 
