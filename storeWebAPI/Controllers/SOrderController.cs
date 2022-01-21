c7b38ad48dd8b6d2e243388d8607de76f4a42e76
Michael Coffey <michaelcoffey1999@gmail.com>
2022-01-21 14:54:57 -0600

    Secondary Order Functions

--- storeWebAPI/Controllers/SOrderController.cs
+++ storeWebAPI/Controllers/SOrderController.cs
@@ -11,46 +11,46 @@ namespace storeWebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class OrderController : ControllerBase
+    public class SOrderController : ControllerBase
     {
 
         private IBL _bl;
         private IMemoryCache _memoryCache;
 
-        public OrderController(IBL bl, IMemoryCache memoryCache)
+        public SOrderController(IBL bl, IMemoryCache memoryCache)
         {
             _bl = bl;
             _memoryCache = memoryCache;
         }
-        // GET: api/<ValuesController>
+        // GET: api/<SOrderController>
         [HttpGet]
-        public List<Order> GetOrdersDate(int id)
+        public List<Order> GetSOrdersDate(int id)
         {
-            return _bl.GetCustomerOrderHistoryDate(id);
+            return _bl.GetStorefrontOrderHistoryDate(id);
         }
 
-        // GET api/<ValuesController>/5
+        // GET api/<SOrderController>/5
         [HttpGet("{id}")]
-        public List<Order> GetOrdersCost(int id)
+        public List<Order> GetSOrdersCost(int id)
         {
-            return _bl.GetCustomerOrderHistoryCost(id);
+            return _bl.GetStorefrontOrderHistoryCost(id);
         }
 
-        // POST api/<ValuesController>
+        // POST api/<SOrderController>
         [HttpPost]
-        public void PostOrders([FromBody] Order nOrder)
+        public void Post([FromBody] Order nOrder)
         {
         }
 
-        // PUT api/<ValuesController>/5
+        // PUT api/<SOrderController>/5
         [HttpPut("{id}")]
-        public void PutIntoOrder(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<ValuesController>/5
+        // DELETE api/<SOrderController>/5
         [HttpDelete("{id}")]
-        public void DeleteOrder(int id)
+        public void Delete(int id)
         {
         }
     }
